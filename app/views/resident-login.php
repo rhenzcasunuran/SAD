2007702c4288c58de28a67e6e5b2cf5fed1bd98f
Contradictive88 <133821754+Contradictive88@users.noreply.github.com>
2023-06-18 08:33:04 +0800

    Add files via upload

--- app/views/resident-login.php
+++ app/views/resident-login.php
@@ -2,70 +2,93 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BRGY Aplaya</title>
+    <title>Resident Login</title>
 
+    <!--BRGY designs-->
     <link rel="stylesheet" href="public/css/bootstrap.min.css">
     <link rel="stylesheet" href="public/css/boxicons.css">
-    <link rel="stylesheet" href="public/css/BRGY-aplaya.css">
+    <link rel="stylesheet" href="public/css/design.css">
+    <link rel="stylesheet" href="public/css/button.css">
 
     <link rel="stylesheet" href="public/css/login.css">
 </head>
 <body>
-    <!--Navbar-->
-    <div class="login-bar" id="top-var">
-        <div class="text-1">
-            Welcome to E-Playa
+    <div id="contentWrapper">
+        <div id="topvarContainer">
+            <div>
+                <div id="brgyName">
+                    BRGY Aplaya
+                </div>
+            </div>
+            <div id="buttonContainer">
+                <div class="text-button createAccount" id="createAccount">
+                    Create Account
+                </div>
+                <div class="filled-button">
+                    Quick Tutorial
+                </div>
+            </div>
         </div>
-        <div class="text-1">
-            About
-        </div>
-    </div>
-    <!--Content-->
-    <div id="content-container">
-        <div class="flex-column justify-content-center d-flex align-items-center w-100">
-            <form action="index.php?page=resident-auth" method="POST" class="form-container needs-validation" id="login-container">
-                <div class="form-header col-12">Resident</div>
-                <div class="form-group col-12 has-feedback">
-                    <label for="username" class="form-label">Username</label>
-                    <input type="text" class="form-control form-field username" id="username" name="username" required>
+        <!--Side Nav-->
+        <div class="flex-column" id="leftSidenav"></div>
+        <div id="rightSidenav"></div>
+        <div class="row" id="contentContainer">
+            <div id="content">
+                <div id="headerWrapper">
+                    <div id="headerContainer">
+                        <div class="header">
+                            Welcome <br> to <br> <span>iLaya</span> 
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group col-12 has-feedback">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control form-field" id="password" name="password" required>
+                <div id="loginWrapper">
+                    <div id="loginContainer">
+                        <form action="index.php?page=resident-auth" method="POST" class="needs-validation" autocomplete="off" novalidate>
+                            <div class="form-header">Resident</div>
+                            <p class="margin">Welcome! Please provide your login credentials to access your account.</p>
+                            <div class="form-group has-feedback">
+                                <input type="text" class="form-control" placeholder="Username" id="username" name="username" required>
+                            </div>
+                            <div class="form-group has-feedback">
+                                <input type="password" class="form-control" placeholder="Password" id="password" name="password" required>
+                            </div>
+                            <span class="margin a resetPassword"> 
+                                Having trouble signing in? 
+                            </span>
+                            <?php
+                                if(isset($_SESSION['error'])) {
+                                    echo '<p class="form-label text-3 text-center text-decoration-none" style="color:red">' . $_SESSION['error'] . '</p>';
+                                    unset($_SESSION['error']); // Clear the error message after displaying it
+                                }
+                            ?>
+                            <div class="form-group justify-content-center align-items-end d-flex">
+                                <input class="filled-button" type="submit" value="Sign in" name="login-btn">
+                            </div>
+                            <label class="form-label margin">Don't have an account?</label>
+                            <span class="a createAccount">
+                                Sign up now!
+                            </span>
+                        </form>
+                    </div>
                 </div>
-                <a href="index.php?page=resident-select-contact" class="form-label text-2 text-center text-decoration-none" id="forgot">Forgot Password?</a>
-                <?php
-                    if(isset($_SESSION['error'])) {
-                        echo '<p class="form-label text-3 text-center text-decoration-none" style="color:red">' . $_SESSION['error'] . '</p>';
-                        unset($_SESSION['error']); // Clear the error message after displaying it
-                    }
-                ?>
-                <div class="form-group col-12 justify-content-center align-items-end d-flex">
-                    <input class="btn text-center" type="submit" value="Login" name="login-btn">
-                </div>
-            </form>
-            <br>
-            <div class="d-flex justify-content-center align-items-center flex-column">
-                <label class="form-label text-3 fw-bold">Don't have an account?</label>
-                <a href="index.php?page=resident-registration">
-                    <button class="btn">Register</button>
-                </a>
             </div>
         </div>
     </div>
+    <!--BRGY scripts-->
     <script src="public/js/bootstrap.bundle.min.js"></script>
     <script src="public/js/jquery-3.6.4.js"></script>
 
+    <script src="public/js/form_validation.js"></script>
+
     <script>
-        //Alphanumeric Validation
-        $('.username').keypress(function (e) {
-          var txt = String.fromCharCode(e.which);
-          if (!txt.match(/[A-Za-z0-9_]/)) {
-              return false;
-          }
-        });
+        $(".createAccount").on('click', function() {
+            location.href = "index.php?page=resident-registration";
+        })
+
+        $(".resetPassword").on('click', function() {
+            location.href = "index.php?page=resident-select-contact";
+        })
     </script>
 </body>
 </html>
